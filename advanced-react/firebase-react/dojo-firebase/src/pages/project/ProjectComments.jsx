51683cfc9ca8b8d3d45136c8df9c4f01f17254ef
umerkang66 <umerkang66@gmail.com>
2023-03-28 13:12:54 +0500

    complete the project

--- advanced-react/firebase-react/dojo-firebase/src/pages/project/ProjectComments.jsx
+++ advanced-react/firebase-react/dojo-firebase/src/pages/project/ProjectComments.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react"
-import { timestamp } from "../../firebase/config"
-import { useAuthContext } from "../../hooks/useAuthContext"
-import { useFirestore } from "../../hooks/useFirestore"
-import Avatar from "../../components/Avatar"
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import { useState } from 'react';
+import { timestamp } from '../../firebase/config';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/Avatar';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 export default function ProjectComments({ project }) {
-  const { user } = useAuthContext()
-  const { updateDocument, response } = useFirestore('projects')
-  const [newComment, setNewComment] = useState('')
+  const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore('projects');
+  const [newComment, setNewComment] = useState('');
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
     const commentToAdd = {
@@ -18,48 +18,54 @@ export default function ProjectComments({ project }) {
       photoURL: user.photoURL,
       content: newComment,
       createdAt: timestamp.fromDate(new Date()),
-      id: Math.random()
-    }
-    
+      id: Math.random(),
+    };
+
     await updateDocument(project.id, {
       comments: [...project.comments, commentToAdd],
-    })
+    });
     if (!response.error) {
-      setNewComment('')
+      setNewComment('');
     }
-  }
+  };
 
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
 
       <ul>
-        {project.comments.length > 0 && project.comments.map(comment => (
-          <li key={comment.id}>
-            <div className="comment-author">
-              <Avatar src={comment.photoURL} />
-              <p>{comment.displayName}</p>
-            </div>
-            <div className="comment-date">
-              <p>{formatDistanceToNow(comment.createdAt.toDate(), {addSuffix: true})}</p>
-            </div>
-            <div className="comment-content">
-              <p>{comment.content}</p>
-            </div>
-          </li>
-        ))}
+        {project.comments.length > 0 &&
+          project.comments.map(comment => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    // this will add a suffix, like ago, 'two mins ago'
+                    addSuffix: true,
+                  })}
+                </p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
       </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
-          <textarea 
-            onChange={(e) => setNewComment(e.target.value)}
+          <textarea
+            onChange={e => setNewComment(e.target.value)}
             value={newComment}
           ></textarea>
         </label>
         <button className="btn">Add Comment</button>
       </form>
     </div>
-  )
+  );
 }
