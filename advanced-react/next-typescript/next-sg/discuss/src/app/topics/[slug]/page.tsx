910ed9e62373b3f893449c78d2d425fc8c019dd0
umerkang66 <umerkang66@gmail.com>
2023-11-24 19:35:52 +0500

    update discus

--- advanced-react/next-typescript/next-sg/discuss/src/app/topics/[slug]/page.tsx
+++ advanced-react/next-typescript/next-sg/discuss/src/app/topics/[slug]/page.tsx
@@ -1,3 +1,19 @@
-export default function TopicShowPage() {
-  return <div>Topic Show</div>;
+interface Props {
+  params: { slug: string };
 }
+
+function TopicShowPage({ params }: Props) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default TopicShowPage;
