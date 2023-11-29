b6878f21c54233bc2c055e89b2f0e9756bab4ed8
Muhammad Umer <umerkang66@gmail.com>
2023-11-29 15:29:41 +0000

    add post show loading

--- advanced-react/next-typescript/next-sg/discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ advanced-react/next-typescript/next-sg/discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -3,7 +3,7 @@ import PostShow from '@/components/posts/post-show';
 import CommentList from '@/components/comments/comment-list';
 import CommentCreateForm from '@/components/comments/comment-create-form';
 import { paths } from '@/paths';
-import { fetchCommentsByPostId } from '@/db/queries/comments';
+import { Suspense } from 'react';
 
 interface PostShowPageProps {
   params: {
@@ -20,7 +20,9 @@ async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {'< '}Back to {slug}
       </Link>
-      <PostShow postId={params.postId} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostShow postId={params.postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
--- advanced-react/next-typescript/next-sg/discuss/src/db/queries/comments.ts
+++ advanced-react/next-typescript/next-sg/discuss/src/db/queries/comments.ts
@@ -1,15 +1,16 @@
 import type { Comment } from '@prisma/client';
+import { cache } from 'react';
 import { db } from '..';
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId },
-    include: { user: { select: { name: true, image: true } } },
-  });
-}
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: { user: { select: { name: true, image: true } } },
+    });
+  }
+);
